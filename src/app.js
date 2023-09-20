7acc6aede1c75a8fe1112e35589aa497c3dd1f18
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2023-09-20 23:19:48 +0300

    copy

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-'use strict';
+"use strict";
+
+const fs = require("fs");
+const COPY = "cp";
+
+function cp(fileToCopy, newFileName) {
+  if (fileToCopy === newFileName) {
+    console.log("Path should be different");
+    process.exit();
+  }
+
+  const fileReader = fs.createReadStream(fileToCopy);
+  const fileWriter = fs.createWriteStream(newFileName);
+
+  fileReader.on("data", function (chunk) {
+    fileWriter.write(chunk);
+  });
+
+  fileReader.on("end", function () {
+    fileWriter.close();
+    console.log("File is successfully copied");
+    process.exit();
+  });
+}
+
+process.stdin.once("data", (data) => {
+  const [command, fileToCopy, newFileName] = data.toString().trim().split(" ");
+
+  if (command !== COPY) {
+    console.log("Unknown command");
+    process.exit();
+  }
+
+  cp(fileToCopy, newFileName);
+});
+
+module.exports = {
+  cp,
+};
